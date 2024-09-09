cb1d0855b0861754bb4e4d68a7b5a339489525cd
Myk <mykcryptodev@gmail.com>
2024-09-09 18:59:12 -0400

    deck of cards gets random numbers from chainlink

--- contracts/test/DeckOfCards.sol
+++ contracts/test/DeckOfCards.sol
@@ -2,18 +2,44 @@
 pragma solidity ^0.8.27;
 
 import {DeckOfCards} from "../src/DeckOfCards.sol";
-import {Test} from "forge-std/Test.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {VRFCoordinatorV2Mock} from "../src/VRFCoordinatorV2Mock.sol";
 
-contract GreeterTest is Test {
+contract DeckOfCardsTest is Test {
     DeckOfCards public deckOfCards;
+    VRFCoordinatorV2Mock public vrfCoordinator;
+    bytes32 private constant KEY_HASH = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
     function setUp() public {
-        greeter = new Greeter("Hello, Hardhat!");
+        vrfCoordinator = new VRFCoordinatorV2Mock(
+            100000000000000000, // fee
+            1000000000 // gas price
+        );
+        uint64 subscriptionId = vrfCoordinator.createSubscription();
+        // fund the subscription
+        vrfCoordinator.fundSubscription(subscriptionId, 1 ether);
+        deckOfCards = new DeckOfCards(
+            subscriptionId,
+            address(vrfCoordinator),
+            KEY_HASH
+        );
+        vrfCoordinator.addConsumer(
+            subscriptionId,
+            address(deckOfCards)
+        );
     }
 
-    function testCreateGreeter() public {
-        assertEq(greeter.greet(), "Hello, Hardhat!");
-        greeter.setGreeting("Hola, mundo!");
-        assertEq(greeter.greet(), "Hola, mundo!");
+    function testGetRandomWords () public {
+        deckOfCards.requestRandomWords();
+        uint256 requestId = deckOfCards.s_requestId();
+        vrfCoordinator.fulfillRandomWords(
+            requestId, 
+            address(deckOfCards)
+        );
+        uint256 firstRandomWord = deckOfCards.s_randomWords(0);
+        uint256 secondRandomWord = deckOfCards.s_randomWords(1);
+        // assert that each random word exists
+        assertTrue(firstRandomWord > 0);
+        assertTrue(secondRandomWord > 0);
     }
 }
--- test/DeckOfCards.test.ts
+++ test/DeckOfCards.test.ts
@@ -1,39 +1,26 @@
-// import { expect, assert } from "chai";
-// import hre from "hardhat";
-// import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
-// import { mine, time } from "@nomicfoundation/hardhat-network-helpers";
-// import { Greeter } from "../typechain-types";
+import { expect, assert } from "chai";
+import hre from "hardhat";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { mine, time } from "@nomicfoundation/hardhat-network-helpers";
+import { DeckOfCards } from "../typechain-types";
 
-// describe("DeckOfCards", function () {
-//   let deployerAccount: SignerWithAddress;
-//   let greeter: Greeter;
+describe("DeckOfCards", function () {
+  let deployerAccount: SignerWithAddress;
+  let deckOfCards: DeckOfCards;
 
-//   beforeEach(async function () {
-//     greeter = await hre.ethers.deployContract(
-//       "Greeter",
-//       ["Hello, Hardhat!"],
-//       deployerAccount,
-//     );
-//     await greeter.waitForDeployment();
-//   });
+  beforeEach(async function () {
+    deckOfCards = await hre.ethers.deployContract(
+      "DeckOfCards",
+      [],
+      deployerAccount,
+    );
+    await deckOfCards.waitForDeployment();
+  });
 
-//   it("Should return the new greeting once it's changed", async function () {
-//     expect(await greeter.greet()).to.equal("Hello, Hardhat!");
-
-//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-//     // Wait until the transaction is mined
-//     await setGreetingTx.wait();
-
-//     expect(await greeter.greet()).to.equal("Hola, mundo!");
-//   });
-
-//   // Showcase test on how to use the Hardhat network helpers library
-//   it("Should mine the given number of blocks", async function () {
-//     const blockNumberBefore = await time.latestBlock();
-
-//     await mine(100);
-
-//     assert.equal(await time.latestBlock(), blockNumberBefore + 100);
-//   });
-// });
+  // it should get a random number
+  it("should get a random number", async function () {
+    const randomNumber = await deckOfCards.requestRandomWords();
+    console.log({ randomNumber });
+    assert.isNumber(randomNumber);
+  });
+});
